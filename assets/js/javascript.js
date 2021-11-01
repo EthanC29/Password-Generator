89dfb63e4e4336c5f598a7fe8ed3c3b0626c392b
Ethan <90936828+EthanC29@users.noreply.github.com>
2021-11-01 18:39:12 -0400

    should be complete but not working

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -104,124 +104,129 @@ var specialCharacter = [
 ];
 var arrayList = [];
 
-if (generateBtn) {
+var generatePassword = function() {
 
-    var generatePassword = function() {
-
-        // password length
-        var passwordLength = function() {
-            var passwordLengthRequest = window.prompt("How many characters long would you like your password to be?");
-            if (passwordLengthRequest < 8 || passwordLengthRequest > 128) {
-                window.alert("Please enter an integer of at least 8 and no more than 128")
-                localStorage.removeItem("passwordLengthRequest");
-                passwordLength();
-            }
-            else if (passwordLengthRequest > 8 && passwordLengthRequest < 128) {
-                window.alert("You chose a length of " + passwordLengthRequest + " characters")
-            }
-            else {
-                window.alert("Please enter an integer of at least 8 and no more than 128")
-                passwordLength();
-            }
+    // password length
+    var passwordLength = function() {
+        var passwordLengthRequest = window.prompt("How many characters long would you like your password to be?");
+        if (passwordLengthRequest < 8 || passwordLengthRequest > 128) {
+            window.alert("Please enter an integer of at least 8 and no more than 128")
+            localStorage.removeItem("passwordLengthRequest");
+            passwordLength();
         }
-        passwordLength();
-
-        // character types
-        var characterUppercase = function() {
-            var characterUppercaseRequest = window.prompt('Would you like to include uppercase characters? Please enter "YES" or "NO"');
-            if (characterUppercaseRequest === "YES" || characterUppercaseRequest === "yes") {
-                window.alert("Uppercase characters will be included");
-            }
-            else if (characterUppercaseRequest === "NO" || characterUppercaseRequest === "no") {
-                window.alert("Uppercase characters will not be included");
-            }
-            else {
-                window.alert("Please enter a valid response");
-                localStorage.removeItem("characterUppercaseRequest");
-                characterUppercase();
-            }
+        else if (passwordLengthRequest > 8 && passwordLengthRequest < 128) {
+            window.alert("You chose a length of " + passwordLengthRequest + " characters")
         }
-        characterUppercase();
-
-        var characterLowercase = function() {
-            var characterLowercaseRequest = window.prompt('Would you like to include lowercase characters? Please enter "YES" or "NO"');
-            if (characterLowercaseRequest === "YES" || characterLowercaseRequest === "yes") {
-                window.alert("Lowercase characters will be included");
-            }
-            else if (characterLowercaseRequest === "NO" || characterLowercaseRequest === "no") {
-                window.alert("Lowercase characters will not be included");
-            }
-            else {
-                window.alert("Please enter a valid response");
-                localStorage.removeItem("characterLowercaseRequest");
-                characterLowercase();
-            }
-        }
-        characterLowercase();
-
-        var characterNumeric = function() {
-            var characterNumericRequest = window.prompt('Would you like to include numbers? Please enter "YES" or "NO"');
-            if (characterNumericRequest === "YES" || characterNumericRequest === "yes") {
-                window.alert("Numbers will be included");
-            }
-            else if (characterNumericRequest === "NO" || characterNumericRequest === "no") {
-                window.alert("Numbers will not be included");
-            }
-            else {
-                window.alert("Please enter a valid response");
-                localStorage.removeItem("characterNumericRequest");
-                characterNumeric();
-            }
-        }
-        characterNumeric();
-
-        var characterSpecial = function() {
-            var characterSpecialRequest = window.prompt('Would you like to include special characters? Please enter "YES" or "NO"');
-            if (characterSpecialRequest === "YES" || characterSpecialRequest === "yes") {
-                window.alert("Special characters will be included");
-            }
-            else if (characterSpecialRequest === "NO" || characterSpecialRequest === "no") {
-                window.alert("Special characters will not be included");
-            }
-            else {
-                window.alert("Please enter a valid response");
-                localStorage.removeItem("characterSpecialRequest");
-                characterSpecial();
-            }
-        }
-        characterSpecial();
-
-        // consolidating a list of all approved arrays
-        if (characterUppercaseRequest) {
-            var arrayList = arrayList.concat(uppercaseCharacter);
-        }
-        if (characterLowercaseRequest) {
-            var arrayList = arrayList.concat(lowercaseCharacter);
-        }
-        if (characterNumericRequest) {
-            var arrayList = arrayList.concat(numericCharacter);
-        }
-        if (characterSpecialRequest) {
-            var arrayList = arrayList.concat(specialCharacter);
-        }
-
-        // pick a random character from the array *Length* times
-        Array.prototype.sample = function(){
-            return this[Math.floor(Math.random()*this.length)];
-        }
-
-        var finalCharacters = [];
-        for (var i = 0; i < passwordLengthRequest.length; i++) {
-            finalCharacters[i] = arrayList.sample();
-        }
-
+        else {
+            window.alert("Please enter an integer of at least 8 and no more than 128")
+            passwordLength();
+        };
+        return;
     }
+    passwordLength();
 
+    // character types
+    var characterUppercase = function() {
+        var characterUppercaseRequest = window.prompt('Would you like to include uppercase characters? Please enter "YES" or "NO"');
+        if (characterUppercaseRequest === "YES" || characterUppercaseRequest === "yes") {
+            window.alert("Uppercase characters will be included");
+        }
+        else if (characterUppercaseRequest === "NO" || characterUppercaseRequest === "no") {
+            window.alert("Uppercase characters will not be included");
+        }
+        else {
+            window.alert("Please enter a valid response");
+            localStorage.removeItem("characterUppercaseRequest");
+            characterUppercase();
+        };
+        return;
+    }
+    characterUppercase();
 
-    generatePassword();
+    var characterLowercase = function() {
+        var characterLowercaseRequest = window.prompt('Would you like to include lowercase characters? Please enter "YES" or "NO"');
+        if (characterLowercaseRequest === "YES" || characterLowercaseRequest === "yes") {
+            window.alert("Lowercase characters will be included");
+        }
+        else if (characterLowercaseRequest === "NO" || characterLowercaseRequest === "no") {
+            window.alert("Lowercase characters will not be included");
+        }
+        else {
+            window.alert("Please enter a valid response");
+            localStorage.removeItem("characterLowercaseRequest");
+            characterLowercase();
+        };
+        return;
+    }
+    characterLowercase();
 
-    console.log(finalCharacters);
+    var characterNumeric = function() {
+        var characterNumericRequest = window.prompt('Would you like to include numbers? Please enter "YES" or "NO"');
+        if (characterNumericRequest === "YES" || characterNumericRequest === "yes") {
+            window.alert("Numbers will be included");
+        }
+        else if (characterNumericRequest === "NO" || characterNumericRequest === "no") {
+            window.alert("Numbers will not be included");
+        }
+        else {
+            window.alert("Please enter a valid response");
+            localStorage.removeItem("characterNumericRequest");
+            characterNumeric();
+        };
+        return;
+    }
+    characterNumeric();
+
+    var characterSpecial = function() {
+        var characterSpecialRequest = window.prompt('Would you like to include special characters? Please enter "YES" or "NO"');
+        if (characterSpecialRequest === "YES" || characterSpecialRequest === "yes") {
+            window.alert("Special characters will be included");
+        }
+        else if (characterSpecialRequest === "NO" || characterSpecialRequest === "no") {
+            window.alert("Special characters will not be included");
+        }
+        else {
+            window.alert("Please enter a valid response");
+            localStorage.removeItem("characterSpecialRequest");
+            characterSpecial();
+        };
+        return;
+    }
+    characterSpecial();
+
+    // consolidating a list of all approved arrays
+    if (characterUppercaseRequest) {
+        arrayList = arrayList.concat(uppercaseCharacter);
+    };
+    if (characterLowercaseRequest) {
+        arrayList = arrayList.concat(lowercaseCharacter);
+    };
+    if (characterNumericRequest) {
+        arrayList = arrayList.concat(numericCharacter);
+    };
+    if (characterSpecialRequest) {
+        arrayList = arrayList.concat(specialCharacter);
+    };
+
+    // pick a random character from the array *Length* times and apply it to a final array
+    // Thanks go to Ben Aubin (https://stackoverflow.com/a/33906108/16940312)
+    Array.prototype.sample = function(){
+        return this[Math.floor(Math.random()*this.length)];
+    };
+
+    var finalCharacters = [];
+    for (var i = 0; i < passwordLengthRequest; i++) {
+        finalCharacters[i] = arrayList.sample();
+    };
+
+    // transforms array into a string without commas
+    let finalString = finalCharacters.join("");
+
+    return (finalString);
 }
+
+
+    
     
 
 
